a9013a529a470f64a32236e0b4a8e2e8c81c598a
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-12-02 00:06:55 +0300

    Make all lambdas constexpr

--- main.cpp
+++ main.cpp
@@ -8,73 +8,73 @@
 
 using namespace std::literals;
 
-int main(int argc, char* argv[]) {
-    auto forEachCharacter = [](std::istream &stream, auto F) {
+constexpr auto forEachCharacter = [](std::istream &stream, auto F) {
+    constexpr auto BUFFER_SIZE = 16*1024;
+    std::array<char, BUFFER_SIZE> buffer;
+
+    uintmax_t accumulator = 0;
+    while (size_t nBytes = (stream.read(buffer.data(), BUFFER_SIZE), stream.gcount())) {
+        if (nBytes == 0) break;
+        F(accumulator, buffer, nBytes);
+    }
+
+    return accumulator;
+};
+
+constexpr auto forEachCharacterCompose = []<typename... F>(std::istream &stream, F... fun) {
+    return []<auto... Idx>(std::istream &stream, std::index_sequence<Idx...>, F... fun) {
         constexpr auto BUFFER_SIZE = 16*1024;
         std::array<char, BUFFER_SIZE> buffer;
 
-        uintmax_t accumulator = 0;
+        std::vector<uintmax_t> accumulators(sizeof...(F));
         while (size_t nBytes = (stream.read(buffer.data(), BUFFER_SIZE), stream.gcount())) {
             if (nBytes == 0) break;
-            F(accumulator, buffer, nBytes);
+            (fun(accumulators[Idx], buffer, nBytes), ...);
         }
 
-        return accumulator;
-    };
+        return accumulators;
+    }(stream, std::make_index_sequence<sizeof...(F)>(), fun...);
+};
 
-    auto forEachCharacterCompose = []<typename... F>(std::istream &stream, F... fun) {
-        return []<auto... Idx>(std::istream &stream, std::index_sequence<Idx...>, F... fun) {
-            constexpr auto BUFFER_SIZE = 16*1024;
-            std::array<char, BUFFER_SIZE> buffer;
-
-            std::vector<uintmax_t> accumulators(sizeof...(F));
-            while (size_t nBytes = (stream.read(buffer.data(), BUFFER_SIZE), stream.gcount())) {
-                if (nBytes == 0) break;
-                (fun(accumulators[Idx], buffer, nBytes), ...);
+constexpr auto bytesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
+    accumulator += nBytes;
+};
+constexpr auto linesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
+    for(char *p = buffer.data(); (p = (char*) memchr(p, '\n', (buffer.data() + nBytes) - p)); ++p)
+        ++accumulator;
+};
+constexpr auto wordsCount = [](auto &accumulator, auto &buffer, auto nBytes) {
+    bool isSpace = false;
+    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++it) {
+        switch (*it) {
+            case '\r':
+            case '\n':
+            case '\t':
+            case '\f':
+            case '\v':
+            case ' ': {
+                if (!isSpace) {
+                    ++accumulator;
+                }
+                isSpace = true;
+                break;
             }
-
-            return accumulators;
-        }(stream, std::make_index_sequence<sizeof...(F)>(), fun...);
-    };
-
-    auto bytesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-        accumulator += nBytes;
-    };
-    auto linesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-        for(char *p = buffer.data(); (p = (char*) memchr(p, '\n', (buffer.data() + nBytes) - p)); ++p)
-            ++accumulator;
-    };
-    auto wordsCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-        bool isSpace = false;
-        for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++it) {
-            switch (*it) {
-                case '\r':
-                case '\n':
-                case '\t':
-                case '\f':
-                case '\v':
-                case ' ': {
-                    if (!isSpace) {
-                        ++accumulator;
-                    }
-                    isSpace = true;
-                    break;
-                }
-                default: {
-                    isSpace = false;
-                }
+            default: {
+                isSpace = false;
             }
         }
-    };
-    auto multibyteCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-        for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++accumulator) {
-            const int next = std::mblen(it, endIt - it);
-            if (next == -1)
-                throw std::runtime_error("strlen_mb(): conversion error");
-            it += next;
-        }
-    };
+    }
+};
+constexpr auto multibyteCount = [](auto &accumulator, auto &buffer, auto nBytes) {
+    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++accumulator) {
+        const int next = std::mblen(it, endIt - it);
+        if (next == -1)
+            throw std::runtime_error("strlen_mb(): conversion error");
+        it += next;
+    }
+};
 
+int main(int argc, char* argv[]) {
     if ((argc == 2 && argv[1][0] == '-') || argc == 3) {
         const char* filename = (argc == 3 ? argv[2] : "stdin");
         std::istream *stream = &std::cin;

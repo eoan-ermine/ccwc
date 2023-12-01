c51bcdb04f377fbf0ef68601a97ae9972c1764cc
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-12-01 20:43:47 +0300

    Implement -c flag

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string_view>
+#include <fstream>
+#include <filesystem>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std::literals;
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        return -1;
+    }
+    if (argv[1] != "-c"sv) {
+        return -1;
+    }
+
+    fs::path filename = argv[2];
+    fs::path absolutePath = fs::current_path() / filename;
+
+    std::error_code ec;
+    if (const auto size = fs::file_size(absolutePath, ec); ec) {
+        std::cout << absolutePath << ": " << ec.message() << '\n';
+    } else {
+        std::cout << size << ' ' << argv[2] << '\n';
+    }
+
     return 0;
 }

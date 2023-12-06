654edff7e865da1b50ee36e835a37f28539fb43a
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-12-06 10:08:11 +0300

    Move algorithms to algorithms.hpp

---
+++ ccwc/algorithms.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <istream>
+#include <array>
+#include <cstdint>
+#include <cstring>
+
+constexpr size_t BUFFER_SIZE = 16 * 1024;
+using AccumulatorType = uintmax_t;
+using BufferType = std::array<char, BUFFER_SIZE>;
+
+template <typename... F>
+constexpr auto forEachCharacter(std::istream &stream, F... fun) {
+    return []<auto... Idx>(std::istream &stream, std::index_sequence<Idx...>, F... fun) {
+        BufferType buffer;
+        std::array<AccumulatorType, sizeof...(F)> accumulators;
+        while (size_t nBytes = (stream.read(buffer.data(), BUFFER_SIZE), stream.gcount())) {
+            if (nBytes == 0) break;
+            (fun(accumulators[Idx], buffer, nBytes), ...);
+        }
+        return accumulators;
+    }(stream, std::make_index_sequence<sizeof...(F)>(), fun...);
+};
+
+constexpr auto bytesCount(AccumulatorType &accumulator, BufferType &buffer, size_t nBytes) {
+    accumulator += nBytes;
+}
+
+constexpr auto linesCount(AccumulatorType &accumulator, BufferType &buffer, size_t nBytes) {
+    for (char *p = buffer.data(); (p = (char *) memchr(p, '\n', (buffer.data() + nBytes) - p)); ++p)
+        ++accumulator;
+};
+
+constexpr auto wordsCount(AccumulatorType &accumulator, BufferType &buffer, size_t nBytes) {
+    bool isSpace = false;
+    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++it) {
+        switch (*it) {
+            case '\r':
+            case '\n':
+            case '\t':
+            case '\f':
+            case '\v':
+            case ' ': {
+                if (!isSpace)
+                    ++accumulator;
+                isSpace = true;
+                break;
+            }
+            default: {
+                isSpace = false;
+            }
+        }
+    }
+};
+
+constexpr auto multibyteCount(AccumulatorType &accumulator, BufferType &buffer, size_t nBytes) {
+    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++accumulator) {
+        const int next = std::mblen(it, endIt - it);
+        if (next == -1)
+            throw std::runtime_error("strlen_mb(): conversion error");
+        it += next;
+    }
+};
--- ccwc/main.cpp
+++ ccwc/main.cpp
@@ -1,64 +1,9 @@
-#include <iostream>
-#include <string_view>
 #include <fstream>
-#include <array>
-#include <cstring>
-#include <cstdint>
+#include <iostream>
+
+#include "algorithms.hpp"
 
 using namespace std::literals;
-
-constexpr auto forEachCharacter = []<typename... F>(std::istream &stream, F... fun) {
-    return []<auto... Idx>(std::istream &stream, std::index_sequence<Idx...>, F... fun) {
-        constexpr auto BUFFER_SIZE = 16 * 1024;
-        std::array<char, BUFFER_SIZE> buffer;
-        std::array<uintmax_t, sizeof...(F)> accumulators;
-        while (size_t nBytes = (stream.read(buffer.data(), BUFFER_SIZE), stream.gcount())) {
-            if (nBytes == 0) break;
-            (fun(accumulators[Idx], buffer, nBytes), ...);
-        }
-        return accumulators;
-    }(stream, std::make_index_sequence<sizeof...(F)>(), fun...);
-};
-
-constexpr auto bytesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-    accumulator += nBytes;
-};
-
-constexpr auto linesCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-    for (char *p = buffer.data(); (p = (char *) memchr(p, '\n', (buffer.data() + nBytes) - p)); ++p)
-        ++accumulator;
-};
-
-constexpr auto wordsCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-    bool isSpace = false;
-    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++it) {
-        switch (*it) {
-            case '\r':
-            case '\n':
-            case '\t':
-            case '\f':
-            case '\v':
-            case ' ': {
-                if (!isSpace)
-                    ++accumulator;
-                isSpace = true;
-                break;
-            }
-            default: {
-                isSpace = false;
-            }
-        }
-    }
-};
-
-constexpr auto multibyteCount = [](auto &accumulator, auto &buffer, auto nBytes) {
-    for (auto it = buffer.data(), endIt = it + nBytes; it != endIt; ++accumulator) {
-        const int next = std::mblen(it, endIt - it);
-        if (next == -1)
-            throw std::runtime_error("strlen_mb(): conversion error");
-        it += next;
-    }
-};
 
 int main(int argc, char *argv[]) {
     std::istream *stream = &std::cin;
